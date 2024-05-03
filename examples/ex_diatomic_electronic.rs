a71de17e8ce577587737e136d0ae89b8d770ee4e
aki <aki@mymail.com>
2024-05-04 02:47:52 +0900

    implemented for electronic spectrum

---
+++ examples/ex_diatomic_electronic.rs
@@ -0,0 +1,40 @@
+use diatomic_simulator::electronic::Population;
+use diatomic_simulator::utl::{convolute_lorentz, out_csv, LineShape};
+use std::error::Error;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let argv = std::env::args().collect::<Vec<String>>();
+    if argv.len() < 7 {
+        panic!("Error: invalid args");
+    }
+
+    let (file_name, temperature, j_max, band_origin, rot_const_ground, rot_const_excited) = (
+        &argv[1],
+        &argv[2].parse::<f64>()?,
+        &argv[3].parse::<i32>()?,
+        &argv[4].parse::<f64>()?,
+        &argv[5].parse::<f64>()?,
+        &argv[6].parse::<f64>()?,
+    );
+    let lorentz_line_shape = LineShape::new(0.04);
+    let spec = Population::new(
+        *temperature,
+        *j_max,
+        *band_origin,
+        *rot_const_ground,
+        *rot_const_excited,
+    );
+
+    let (signal_raw_x, signal_raw_y) = spec.calc_spectrum();
+
+    let (x_signal, y_signal) = convolute_lorentz(
+        1000.0,
+        1400.0,
+        0.01,
+        &lorentz_line_shape,
+        (&signal_raw_x, &signal_raw_y),
+    );
+    out_csv(&x_signal, &y_signal, file_name)?;
+
+    Ok(())
+}
--- src/electronic.rs
+++ src/electronic.rs
@@ -1,14 +1,14 @@
 use crate::utl::DiatomicMolecule;
 
-pub struct Populatoin {
+pub struct Population {
     pub temperature: f64,
     pub j_max: i32,
     pub band_origin: f64,
-    molecule_grownd: DiatomicMolecule,
+    molecule_ground: DiatomicMolecule,
     molecule_excited: DiatomicMolecule,
 }
 
-impl Populatoin {
+impl Population {
     pub fn new(
         temperature: f64,
         j_max: i32,
@@ -20,25 +20,44 @@ impl Populatoin {
             temperature,
             j_max,
             band_origin,
-            molecule_grownd: DiatomicMolecule::new(rot_const_ground),
+            molecule_ground: DiatomicMolecule::new(rot_const_ground),
             molecule_excited: DiatomicMolecule::new(rot_const_excited),
         }
     }
 
     pub fn update_rot_cosnt(&mut self, rot_const_ground: f64, rot_const_excited: f64) {
-        self.molecule_grownd.rot_const = rot_const_ground;
+        self.molecule_ground.rot_const = rot_const_ground;
         self.molecule_excited.rot_const = rot_const_excited;
     }
 
-    /*
-    pub fn spectrum(&self) -> Vec<(f64, f64)> {
-        let mut signal_x = vec![0.0; self.j_max as usize + 1];
-        let mut partition_func = 0.0;
+    pub fn calc_spectrum(&self) -> (Vec<f64>, Vec<f64>) {
+        let mut signal_x = vec![];
+        let mut signal_y = vec![];
+        let (mut partition_func_ground, mut partition_func_excited) = (0.0, 0.0);
+
+        for j in 0..=self.j_max {
+            partition_func_ground +=
+                (2.0 * j as f64 + 1.0) * (-self.molecule_ground.energy(j) / self.temperature).exp();
+            partition_func_excited += (2.0 * j as f64 + 1.0)
+                * (-self.molecule_excited.energy(j) / self.temperature).exp();
+        }
 
         for j in 0..=self.j_max {
             for delta_j in [-1, 0, 1] {
+                let (energy_ground, energy_excited) = (
+                    self.molecule_ground.energy(j),
+                    self.molecule_excited.energy(j + delta_j),
+                );
+                signal_x.push(energy_excited - energy_ground + self.band_origin);
+                signal_y.push(
+                    (j as f64 + 1.0) / (2.0 * j as f64 + 1.0)
+                        * ((-energy_excited / self.temperature).exp() / partition_func_excited
+                            - (-energy_ground / self.temperature).exp() / partition_func_ground)
+                            .abs(),
+                );
             }
         }
+
+        (signal_x, signal_y)
     }
-    */
 }
