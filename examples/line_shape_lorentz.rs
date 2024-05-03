daee9be484796037f685b863a7b031df8ff37375
aki <aki@mymail.com>
2024-05-04 01:31:37 +0900

    Implementation to add Lorentz width

---
+++ examples/line_shape_lorentz.rs
@@ -0,0 +1,23 @@
+use diatomic_simulator::utl::{convolute_lorentz, out_csv, LineShape};
+use std::error::Error;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let argv = std::env::args().collect::<Vec<String>>();
+    if argv.len() < 3 {
+        panic!("Error: invalid args");
+    }
+    let (file_name, lorentz_width) = (&argv[1], &argv[2].parse::<f64>()?);
+
+    let lorentz_line_shape = LineShape::new(*lorentz_width);
+    let (x_signal_raw, y_signal_raw) = (vec![1.2, 2.3, 3.8, 4.1], vec![6.0, 2.5, 8.1, 5.1]);
+    let (x_signal, y_signal) = convolute_lorentz(
+        0.0,
+        5.0,
+        0.01,
+        &lorentz_line_shape,
+        (&x_signal_raw, &y_signal_raw),
+    );
+
+    out_csv(&x_signal, &y_signal, file_name)?;
+    Ok(())
+}
--- src/utl.rs
+++ src/utl.rs
@@ -1,3 +1,7 @@
+use std::error::Error;
+use std::fs::File;
+use std::io::Write;
+
 pub struct DiatomicMolecule {
     pub rot_const: f64,
 }
@@ -10,4 +14,60 @@ impl DiatomicMolecule {
     pub fn energy(&self, j: i32) -> f64 {
         self.rot_const * (j * j + j) as f64
     }
+}
+
+pub struct LineShape {
+    width_lorentz: f64,
+}
+
+impl LineShape {
+    pub fn new(width_lorentz: f64) -> Self {
+        Self { width_lorentz }
+    }
+
+    pub fn lorentz(&self, x: f64, x_centor: f64) -> f64 {
+        let pi = std::f64::consts::PI;
+        (self.width_lorentz / 2.0 * pi)
+            / ((x - x_centor).powi(2) + (self.width_lorentz / 2.0).powi(2))
+    }
+}
+
+pub fn convolute_lorentz(
+    x_ini: f64,
+    x_fin: f64,
+    x_step: f64,
+    line_profile: &LineShape,
+    raw_signal: (&Vec<f64>, &Vec<f64>),
+) -> (Vec<f64>, Vec<f64>) {
+    let len_signal = ((x_fin - x_ini) / x_step) as usize;
+    let x_signal = (0..len_signal)
+        .map(|x| x_ini + x as f64 * x_step)
+        .collect::<Vec<f64>>();
+    let mut y_signal = vec![0.0; len_signal];
+
+    for i in 0..raw_signal.0.len() {
+        for j in 0..x_signal.len() {
+            y_signal[j] += raw_signal.1[i] * line_profile.lorentz(x_signal[j], raw_signal.0[i]);
+        }
+    }
+
+    (x_signal, y_signal)
+}
+
+pub fn out_csv(
+    x_data: &Vec<f64>,
+    y_data: &Vec<f64>,
+    path_to_file: &str,
+) -> Result<(), Box<dyn Error>> {
+    if x_data.len() != y_data.len() {
+        panic!("Error: lenght of two vectors is not equal");
+    }
+
+    let mut file = File::create(path_to_file)?;
+
+    for (x, y) in x_data.iter().zip(y_data.iter()) {
+        writeln!(file, "{x}, {y}")?;
+    }
+
+    Ok(())
 }
