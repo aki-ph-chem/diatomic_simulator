94aa97c09a4be19ed8dd43c21fc4a78499d70834
aki <aki@mymail.com>
2024-05-05 01:01:40 +0900

    main screen looks ok for now

--- gui/ui_mw.rs
+++ gui/ui_mw.rs
@@ -6,8 +6,48 @@ fn main() -> Result<(), Box<dyn Error>> {
     gtk::init()?;
     let ui = include_str!("ui/ui_mw.ui");
     let builder = gtk::Builder::from_string(ui);
-    let window: gtk::Window = builder.object("window").expect("Error: window");
 
+    // generate window
+    let window: gtk::Window = builder.object("window").expect("Error: window");
+    window.connect_delete_event(|_, _| {
+        gtk::main_quit();
+        gtk::glib::Propagation::Stop
+    });
+
+    // quit program by "Quit" button
+    let quit: gtk::MenuItem = builder.object("quit").expect("Error: quit");
+    quit.connect_activate(move |_| {
+        gtk::main_quit();
+    });
+    // quit program by Ctrl + Q
+    let accel_group = gtk::AccelGroup::new();
+    window.add_accel_group(&accel_group);
+    let (key, modifier) = gtk::accelerator_parse("<Primary>Q");
+    quit.add_accelerator(
+        "activate",
+        &accel_group,
+        key,
+        modifier,
+        gtk::AccelFlags::VISIBLE,
+    );
+
+    // const menu
+    let const_menu: gtk::MenuItem = builder.object("const").expect("Error: const");
+
+    // generate About dialog by click "About" button
+    let about: gtk::MenuItem = builder.object("about").expect("Error: about");
+    let about_dialog: gtk::AboutDialog =
+        builder.object("about_dialog").expect("Error: about_dialog");
+    let window_ = window.clone();
+    about.connect_activate(move |_| {
+        about_dialog.set_title("About");
+        about_dialog.set_authors(&["Aki"]);
+        about_dialog.set_transient_for(Some(&window_));
+        about_dialog.run();
+        about_dialog.hide();
+    });
+
+    // show window & enter event loop
     window.show_all();
     gtk::main();
 
